c4c96391a97c6c8f6bd324d505858e13183c68f5
Tushar Anekar <anekar.tushar@gmail.com>
2023-09-18 13:14:20 +0530

    feat- header added

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { HeaderProps } from "../../types/HeaderTypes"
+
+const Header: React.FC<HeaderProps> = ({title}) => {
+    return(
+        <header>
+            <h1>{title}</h1>
+        </header>
+    )
+}
+
+export {Header}
